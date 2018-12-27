742785fe93df1b9db7ea1434a61a6ee01714dbd0
Darling <darling.chavez@hotmail.com>
2018-12-27 07:44:11 -0500

    seed data

--- Escuela/Migrations/Configuration.cs
+++ Escuela/Migrations/Configuration.cs
@@ -5,6 +5,7 @@ namespace Escuela.Migrations
     using System.Data.Entity.Migrations;
     using System.Linq;
     using Escuela.Models;
+    using System.Collections.Generic;
 
     internal sealed class Configuration : DbMigrationsConfiguration<DbContextEscuela>
     {
@@ -20,7 +21,32 @@ namespace Escuela.Migrations
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
             //  to avoid creating duplicate seed data.
 
-            System.Collections.Generic.IList<Curso> cursos = new System.Collections.Generic.List<Curso>();
+            if (!context.Curso.Any())
+            {
+                IList<Curso> cursos = GetListCursos();
+                context.Curso.AddRange(cursos);
+                context.SaveChanges();
+            }
+            if (!context.Representante.Any())
+            {
+                IList<Representante> representantes = GetListRepresentantes();
+                context.Representante.AddRange(representantes);
+                context.SaveChanges();
+            }
+            if (!context.Estudiante.Any())
+            {
+                IList<Estudiante> estudiantes = GetListEstudiantes();
+                context.Estudiante.AddRange(estudiantes);
+                context.SaveChanges();
+            }
+
+            //base.Seed(context);
+
+        }
+
+        private IList<Curso> GetListCursos()
+        {
+            IList<Curso> cursos = new List<Curso>();
             cursos.Add(new Curso()
             {
                 Descripcion = "Octavo de basica",
@@ -32,7 +58,7 @@ namespace Escuela.Migrations
             cursos.Add(new Curso()
             {
                 Descripcion = "Noveno de basica",
-                Especializacion = GlobalWords.Especializacion.Basica,                
+                Especializacion = GlobalWords.Especializacion.Basica,
                 Nivel = 9,
                 Paralelo = "B",
                 Estado = GlobalWords.Status.Activo
@@ -69,10 +95,79 @@ namespace Escuela.Migrations
                 Paralelo = "A",
                 Estado = GlobalWords.Status.Activo
             });
-            context.Curso.AddRange(cursos);
-            context.SaveChanges();
-            //base.Seed(context);
+            return cursos;
+        }
 
+        private IList<Representante> GetListRepresentantes()
+        {
+            IList<Representante> representantes = new List<Representante>()
+            {
+                new Representante()
+                {
+                    Apellido = "Hernandez",
+                    Nombre = "Joaquim",
+                    Cedula = "0913674536",
+                    Direccion = "Tarrasa Espanha",
+                    Email = "padredexavi@gmail.com",
+                    Password = "123",
+                    Telefono = "09123",
+                    Genero = GlobalWords.Gender.Masculino,
+                    FechaCumpleanhos = new DateTime(1947,7,26),
+                    Estado = GlobalWords.Status.Activo
+                },
+                new Representante()
+                {
+                    Apellido = "Iniesta",
+                    Nombre = "Jose Antonio",
+                    Cedula = "0913478956",
+                    Email = "padredeiniesta@gmail.com",
+                    Password = "123",
+                    Direccion = "FuenteAlbilla Espanha",
+                    Telefono = "09321",
+                    FechaCumpleanhos = new DateTime(1984,5,11),
+                    Genero = GlobalWords.Gender.Masculino,
+                    Estado = GlobalWords.Status.Activo
+                }
+            };
+            return representantes;
+        }
+
+        private IList<Estudiante> GetListEstudiantes()
+        {
+            IList<Estudiante> estudiantes = new List<Estudiante>()
+            {
+                new Estudiante()
+                {
+                    Apellido = "Hernandez",
+                    Nombre = "Xavi",
+                    Cedula = "0923456423",
+                    IdCurso = 1,
+                    IdRepresentante = 1,
+                    Direccion = "Tarrasa Espanha",
+                    Email = "elxavidelbarza@gmail.com",
+                    Password = "123",
+                    Telefono = "09123",
+                    Genero = GlobalWords.Gender.Masculino,
+                    FechaCumpleanhos = new DateTime(1980,1,25),
+                    Estado = GlobalWords.Status.Activo
+                },
+                new Estudiante()
+                {
+                    Apellido = "Iniesta",
+                    Nombre = "Andres",
+                    Cedula = "0924324532",
+                    IdCurso = 2,
+                    IdRepresentante = 2,
+                    Email = "elpeladoiniesta@gmail.com",
+                    Password = "123",
+                    Direccion = "FuenteAlbilla Espanha",
+                    Telefono = "09321",
+                    FechaCumpleanhos = new DateTime(1984,5,11),
+                    Genero = GlobalWords.Gender.Masculino,
+                    Estado = GlobalWords.Status.Activo
+                }
+            };
+            return estudiantes;
         }
     }
 }
